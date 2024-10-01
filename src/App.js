b44eb2590b99d8661736bafd8fbedc5b98e03a6b
saranya <saranyanambi911@gmail.com>
2024-10-01 14:35:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from "./Components/Home/Home"
+import Example from "./Components/Piechart/Piechart";
+import React from 'react';
 function App() {
+  console.log('react',React.version)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+   <h1>hi</h1>
+    {/* <Example/> */}
     </div>
   );
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,41 @@
+import { useState,useEffect } from "react";
+import "./Home.css";
+import Wallet from "../Wallet/Wallet";
+// import ExpenseTable from "../ExpenseTable/ExpenseTable"
+// import LineChart from "../LineChart/LineChart";
+const Home=()=>{
+
+    const categories = [
+        "Food",
+        "Grocery",
+        "Travel",
+        "Entertainment",
+        "Shopping",
+        "Others",
+      ];
+    const[Balance,setBalance]=useState(
+        localStorage.getItem("Balance")?parseInt(localStorage.getItem("Balance"),10):5000);
+   
+
+        const[expense,setExepense]=useState([]);
+
+useEffect(() => {
+    const storedExpenses = localStorage.getItem("Expense");
+    if (storedExpenses) {
+        setExepense(JSON.parse(storedExpenses));
+    }
+}, []);
+
+const handleexpenseUpdate=(expense)=>{
+    setExepense(expense);
+    localStorage.setItem("Expense",JSON.stringify(expense));
+};
+    console.log(expense);
+    return(
+        <div className="total-conatiner">
+        <Wallet categories={categories} Balance={Balance} setBalance={setBalance} expense={expense} setExepense={setExepense} handleexpenseUpdate={handleexpenseUpdate}/>
+       
+        </div>
+    )
+}
+export default Home;
